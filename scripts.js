15531589dbaf62dacf36347a472eef40f647b65a
stanciudrg <stanciudragos1337@gmail.com>
2023-08-09 16:07:39 +0300

    Show format requirements on input focus

--- scripts.js
+++ scripts.js
@@ -1,3 +1,35 @@
+document.querySelector('input[type=text').addEventListener('focus', (e) => {
+    document.querySelector('.name-format').classList.toggle('visible');
+})
+
+document.querySelector('input[type=text').addEventListener('blur', (e) => {
+    document.querySelector('.name-format').classList.toggle('visible');
+})
+
+document.querySelector('input[type=email').addEventListener('focus', (e) => {
+    document.querySelector('.email-format').classList.toggle('visible');
+})
+
+document.querySelector('input[type=email').addEventListener('blur', (e) => {
+    document.querySelector('.email-format').classList.toggle('visible');
+})
+
+document.querySelector('input[type=tel').addEventListener('focus', (e) => {
+    document.querySelector('.phone-format').classList.toggle('visible');
+})
+
+document.querySelector('input[type=tel').addEventListener('blur', (e) => {
+    document.querySelector('.phone-format').classList.toggle('visible');
+})
+
+document.querySelector('input[type=password').addEventListener('focus', (e) => {
+    document.querySelector('.password-format').classList.toggle('visible');
+})
+
+document.querySelector('input[type=password').addEventListener('blur', (e) => {
+    document.querySelector('.password-format').classList.toggle('visible');
+})
+
 document.querySelector('#light').addEventListener('change', (e) => {
     if (e.target.checked) {
         const root = document.documentElement;
