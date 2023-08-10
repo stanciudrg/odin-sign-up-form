0954187f652b734de1b9806f02073b2047ef4d41
stanciudrg <stanciudragos1337@gmail.com>
2023-08-10 15:56:04 +0300

    Improve accessibility for sighted and unsighted with WAI-ARIA

--- scripts.js
+++ scripts.js
@@ -1,34 +1,39 @@
-document.querySelector('input[type=text').addEventListener('focus', (e) => {
-    document.querySelector('.name-format').classList.toggle('visible');
+const nameInput = document.querySelector('input[type=text');
+const emailInput = document.querySelector('input[type=email');
+const phoneInput = document.querySelector('input[type=tel');
+const passwordInput = document.querySelector('input[type=password');
+
+nameInput.addEventListener('focus', (e) => {
+    document.querySelector('#name-format').style = "transform: scale(1)";
 })
 
-document.querySelector('input[type=text').addEventListener('blur', (e) => {
-    document.querySelector('.name-format').classList.toggle('visible');
+nameInput.addEventListener('blur', (e) => {
+    document.querySelector('#name-format').style = "transform: scale(0)";
+});
+
+emailInput.addEventListener('focus', (e) => {
+    document.querySelector('#email-format').style = "transform: scale(1)";
 })
 
-document.querySelector('input[type=email').addEventListener('focus', (e) => {
-    document.querySelector('.email-format').classList.toggle('visible');
+emailInput.addEventListener('blur', (e) => {
+    document.querySelector('#email-format').style = "transform: scale(0)";
+});
+
+phoneInput.addEventListener('focus', (e) => {
+    document.querySelector('#name-format').style = "transform: scale(1)";
 })
 
-document.querySelector('input[type=email').addEventListener('blur', (e) => {
-    document.querySelector('.email-format').classList.toggle('visible');
+phoneInput.addEventListener('blur', (e) => {
+    document.querySelector('#phone-format').style = "transform: scale(0)";
+});
+
+passwordInput.addEventListener('focus', (e) => {
+    document.querySelector('#password-format').style = "transform: scale(1)";
 })
 
-document.querySelector('input[type=tel').addEventListener('focus', (e) => {
-    document.querySelector('.phone-format').classList.toggle('visible');
-})
-
-document.querySelector('input[type=tel').addEventListener('blur', (e) => {
-    document.querySelector('.phone-format').classList.toggle('visible');
-})
-
-document.querySelector('input[type=password').addEventListener('focus', (e) => {
-    document.querySelector('.password-format').classList.toggle('visible');
-})
-
-document.querySelector('input[type=password').addEventListener('blur', (e) => {
-    document.querySelector('.password-format').classList.toggle('visible');
-})
+passwordInput.addEventListener('blur', (e) => {
+    document.querySelector('#password-format').style = "transform: scale(0)";
+});
 
 document.querySelector('#light').addEventListener('change', (e) => {
     if (e.target.checked) {
