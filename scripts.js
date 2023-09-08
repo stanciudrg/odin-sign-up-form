ea1c12ef2ca0e387278cfef8199f8b75b31bd3b9
stanciudrg <stanciudragos1337@gmail.com>
2023-09-08 12:20:53 +0300

    Organize and structure the code using comments

--- scripts.js
+++ scripts.js
@@ -7,7 +7,10 @@ const formatPhoneNumber = document.querySelector('input[type=tel').addEventListe
 const validatePasswordInputs = document.querySelectorAll('input[type=password]').forEach(input => validate(input));
 const matchPasswordInputs = document.querySelectorAll('input[type=password]').forEach(input => input.addEventListener('input', matchPasswords))
 
+
 function validate(input) {
+
+    // Shows the input format only while the input is being focused
 
     input.addEventListener('focus', (e) => {
         input.parentElement.parentElement.lastElementChild.style = "transform: scale(1)";
@@ -16,6 +19,8 @@ function validate(input) {
     input.addEventListener('blur', (e) => {
 
         input.parentElement.parentElement.lastElementChild.style = "transform: scale(0)";
+
+        // Shows the validity status icons when the blur event is triggered
 
         if ((e.target.validity.valid == true || e.target.validity.valid == false) && e.target.value !== "") {
             e.target.nextElementSibling.style = "transform: scale(1)";
@@ -27,17 +32,26 @@ function validate(input) {
 
         if (input.id == 'email') {
 
+            // Replaces the error message that is being shown when an incorrect format is 
+            // used for the email address input 
+
             input.validity.typeMismatch ?
                 input.setCustomValidity("Please match the requested format.") :
                 input.setCustomValidity('');
 
         }
 
+        // Hide the validity status icon if user deletes the entire content of an input
+
         if (e.target.value === "") {
             e.target.nextElementSibling.style = "transform: scale(0)";
         }
 
     })
+
+    // Show the validity status icon on Enter keyboard press or Enter key touch. This ensures that
+    // the validity status icon is shown if the user tries to move forward to the next input by pressing Enter
+    // but is not allowed due to the format being incorrect
 
     input.addEventListener('invalid', (e) => {
         if (e.target.value !== "") {
@@ -48,6 +62,9 @@ function validate(input) {
 }
 
 function format(e) {
+
+    // Only allow the user to enter numbers while automatically inserting hyphens in order to respect
+    // the format requested by the pattern attribute set on the phone input
 
     let phoneNumber = e.target.value.replace(/\D/g, '').match(/(\d{0,3})(\d{0,3})(\d{0,4})/);
     e.target.value = !phoneNumber[2] ? phoneNumber[1] : phoneNumber[1] + '-' + phoneNumber[2] + (phoneNumber[3] ? '-' + phoneNumber[3] : '');
